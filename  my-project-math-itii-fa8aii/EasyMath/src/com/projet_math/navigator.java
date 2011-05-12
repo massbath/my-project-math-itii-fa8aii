b699138b15db35634bef0996200794d7f9d486eb
clementvassant <clementvassant@e68b0687-1596-de4f-0244-b7e56723d26d>
2011-05-12 19:47:19 +0000

    New version with a new class navigator, which will be used to open a url containing a course. A progressDialog is showed during the loading of the page. So easy !!!

---  my-project-math-itii-fa8aii/EasyMath/src/com/projet_math/navigator.java
+++  my-project-math-itii-fa8aii/EasyMath/src/com/projet_math/navigator.java
@@ -1,44 +1,67 @@
 package com.projet_math;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.util.Log;
+import android.view.Window;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 public class navigator extends Activity
-{
-	
-	WebView webview;
+{private WebView webview;
+private ProgressDialog progressBar;
 
-	public void OnCreate(Bundle SavedInstance)
+@Override
+protected void onCreate(Bundle savedInstanceState) 
 	{
-		super.onCreate(SavedInstance);
-		setContentView(R.layout.affichage_cours);
-		webview =(WebView) findViewById(R.id.webview);
-		webview.setWebViewClient(new myWebViewClient());
-		webview.getSettings().setJavaScriptEnabled(true);
-		webview.loadUrl("http://forum.frandroid.com/");
-		
-		Toast.makeText(this, "webview", Toast.LENGTH_SHORT).show();
-	}
-	
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
-			webview.goBack();
-			return true;
-		}
-		return super.onKeyDown(keyCode, event);
-	}
-	
-	private class myWebViewClient extends WebViewClient 
-		{
-		@Override
-		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-			view.loadUrl(url);
-			return true;
-		}
+	super.onCreate(savedInstanceState);
+	 
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+    setContentView(R.layout.affichage_cours);
+    Bundle urlToOpen= this.getIntent().getExtras();
+    webview = (WebView)findViewById(R.id.webview);
+
+    WebSettings settings = webview.getSettings();
+    settings.setJavaScriptEnabled(true);
+    webview.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+
+    final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
+    progressBar = ProgressDialog.show(this, "Veuillez patienter", "Chargement ...");
+
+    webview.setWebViewClient(new WebViewClient() {
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+           
+            view.loadUrl(url);
+            return true;
+        }
+
+        public void onPageFinished(WebView view, String url) {
+           
+            if (progressBar.isShowing()) {
+                progressBar.dismiss();
+            }
+        }
+
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+           
+           // Toast.makeText(this, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            alertDialog.setTitle("Error");
+            alertDialog.setMessage(description);
+            alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    return;
+                }
+            });
+            alertDialog.show();
+        }
+    });
+    webview.loadUrl(urlToOpen.getString("url"));
 }
 }
